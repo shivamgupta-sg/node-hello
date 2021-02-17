bd3b96fdf0edf77df9e5cab30591789f15fe57fd
shivamgupta-sg <76810109+shivamgupta-sg@users.noreply.github.com>
2021-02-17 15:27:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port2 = process.env.PORT2 || 3001;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node! from shivam\n'
+  const msg = 'Hello Node! from shivam gupta\n'
   res.end(msg);
 });
 
